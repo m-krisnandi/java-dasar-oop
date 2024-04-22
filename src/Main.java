a3235e987e19b92aeba6f2b88e10e44935a9d7fa
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-04-22 21:59:14 +0700

    create class, object and filed

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        var person1 = new Person();
+
+        person1.name = "Muhamad Krisnandi";
+        person1.address = "Bandung";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        Person person2 = new Person();
+    }
+}
