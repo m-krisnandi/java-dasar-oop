8aaaf9d22ff4497c151a621402f5d098e3adab1b
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-05-07 21:57:55 +0700

    equals() and hashCode() method

--- src/ecommerce/data/Product.java
+++ src/ecommerce/data/Product.java
@@ -1,5 +1,7 @@
 package ecommerce.data;
 
+import java.util.Objects;
+
 public class Product {
     public String name;
     public int price;
@@ -12,5 +14,23 @@ public class Product {
     @Override
     public String toString() {
         return "Product name: " + name + ", Price: " + price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (price != product.price) return false;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + price;
+        return result;
     }
 }
--- src/ecommerce/data/ProductApp.java
+++ src/ecommerce/data/ProductApp.java
@@ -8,5 +8,9 @@ public class ProductApp {
         System.out.println(product.price);
 
         System.out.println(product);
+
+        Product product2 = new Product("Mackbook Pro", 30_000_000);
+        System.out.println(product.equals(product2));
+        System.out.println(product.hashCode() == product2.hashCode());
     }
 }
